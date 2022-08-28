1f8abce43e41754a50498121b885c254ca3419e4
frontendry <brngatarwa@gmail.com>
2022-08-28 20:58:58 +0300

    indicator change

--- src/components/Indicators/Indicators.tsx
+++ src/components/Indicators/Indicators.tsx
@@ -2,11 +2,11 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const UpIndicator = () => {
-  return <i className="bi bi-chevron-up"></i>;
+  return <i className="bi bi-arrow-up"></i>;
 };
 
 const DownIndicator = () => {
-  return <i className="bi bi-chevron-down"></i>;
+  return <i className="bi bi-arrow-down"></i>;
 };
 
 export { UpIndicator, DownIndicator };
